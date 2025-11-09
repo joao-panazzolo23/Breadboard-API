d06325d8c6bc29a3d9e1abfe0c1b139e1d808506
Barba <hated.kuroitsuki@gmail.com>
2025-11-08 21:42:30 -0300

    fix: several fixes

--- src/Application/Breadboard.Application/Program.cs
+++ src/Application/Breadboard.Application/Program.cs
@@ -1,6 +1,37 @@
+using Breadboard.Application.Extensions;
+using Breadboard.Infra.PostgreSQL.Extensions;
+
+//TODO: DESCOBRIR OQ EH OPEN API E PQ USAR ESSA BOMBA EM .NET 9
 var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddOpenApi();
+builder.Services.AddSwaggerExtensions();
+builder.Services.AddApiVersion();
+builder.Services.AddCaching();
+builder.AddControllerNamingConvention();
+//services.AddHttpContextAccessor(); later
+builder.Services.ConfigureJsonOptions();
+
+//entity dependencies & repositories
+builder.Services.AddEntityFrameWork(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//create database & apply pending migrations
+app.Services.EnsureDbCreation();
+app.UseRouting();
+app.AddSwaggerConfiguration();
+app.MapEndpoints();
+//authenticate & authorize
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
+
+app.UseHttpsRedirection();
 
 app.Run();
